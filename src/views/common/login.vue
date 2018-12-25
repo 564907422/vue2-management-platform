3d85a8e8ac2d1bbcdde0d756baf1a31a945a118d
xiaozq <xiaozq@bbtree.com>
2018-12-25 12:50:53 +0800

    init

---
+++ src/views/common/login.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="login-box">
+    <div class="logo-box">
+      <h1 class="logo" title="智慧树">智慧树</h1>
+    </div>
+    <div class="title-box">
+      <h2>智慧树paas管理平台</h2>
+      <p></p>
+    </div>
+    <div class="login-main-box">
+      <div class="login-main clearfix">
+        <div class="login-main-img fl"></div>
+        <div class="login-main-form-box fr">
+          <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()"
+                   status-icon>
+            <el-form-item prop="userName">
+              <el-input v-model="dataForm.userName" placeholder="用户名" prefix-icon="user-name-icon"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input v-model="dataForm.password" type="password" placeholder="密码"
+                        prefix-icon="password-icon"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button class="login-btn-submit" type="primary" @click="dataFormSubmit()">登录</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // import {getUUID} from '@/utils'
+
+  export default {
+    data() {
+      return {
+        dataForm: {
+          userName: '',
+          password: ''
+        },
+        dataRule: {
+          userName: [
+            {required: true, message: '帐号不能为空', trigger: 'blur'}
+          ],
+          password: [
+            {required: true, message: '密码不能为空', trigger: 'blur'}
+          ]
+        }
+      }
+    },
+    created() {
+    },
+    methods: {
+      // 提交表单
+      dataFormSubmit() {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl('/paas/manager/login'),
+              method: 'post',
+              data: this.$http.adornData({
+                'username': this.dataForm.userName,
+                'password': this.dataForm.password
+              })
+            }).then(({data}) => {
+              if (data && data.code === '10000') {
+                this.$router.replace({name: 'table'})
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $NormalColor: #2F54EB;
+  $hoverColor: #597EF7;
+  $clickColor: #1D39C4;
+  .login-box {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #F2F6FC;
+    overflow: hidden;
+    .logo-box {
+      text-align: center;
+      margin: 56px 0 16px 0;
+      h1 {
+        margin: 0;
+      }
+      .logo {
+        text-indent: -9999px;
+        display: inline-block;
+        width: 110px;
+        height: 44px;
+        /*background: url("../../assets/img/logo.png") no-repeat center 100%;*/
+      }
+    }
+    .title-box {
+      text-align: center;
+      h2 {
+        font-size: 32px;
+        color: rgba(48, 49, 51, 0.85);
+        margin: 0;
+      }
+      p {
+        font-size: 16px;
+        color: #909399;
+        margin: 0;
+        padding: 8px 0 16px 0;
+      }
+    }
+    .login-main-box {
+      text-align: center;
+      .login-main {
+        display: inline-block;
+        width: 660px;
+        height: 350px;
+        background: #FFFFFF;
+        box-shadow: 0 4px 20px 0 #D3DAE8;
+        border-radius: 16px;
+        @media screen and (max-width: 576px) {
+          height: 100%;
+          width: 500px;
+        }
+
+        .login-main-img {
+          display: inline-block;
+          height: 350px;
+          width: 312px;
+          /*background: url("../../assets/img/login_icon.png") no-repeat center 100%;*/
+          @media screen and (max-width: 576px) {
+            display: block;
+            float: none;
+            width: 100%;
+          }
+        }
+        .login-main-form-box {
+          margin: 40px 40px 0 0;
+          display: inline-block;
+          @media screen and (max-width: 576px) {
+            display: block;
+            float: none;
+            width: 268px;
+            margin: 0 auto;
+          }
+          .no-password-box {
+            padding-top: 8px;
+            text-align: right;
+            & .link-span {
+              cursor: pointer;
+              color: $NormalColor;
+              &:hover {
+                color: $hoverColor;
+              }
+            }
+          }
+          .login-btn-submit {
+            width: 100%;
+          }
+          .code-input-box {
+            width: 172px;
+          }
+          .code-img-box {
+            display: inline-block;
+            width: 88px;
+            height: 40px;
+            border: 1px solid #d9d9d9;
+            border-radius: 4px;
+            img {
+              width: 100%;
+              height: inherit;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
---
+++ src/views/table/table.vue
@@ -0,0 +1,209 @@
+<template>
+  <section>
+    <el-row>
+      <el-col :span="24">
+        <!--表单-->
+        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+          <el-form-item label="服务标识" prop="serviceId">
+            <el-input v-model="formInline.user.serviceId" placeholder="服务标识" style="width: 140px;"></el-input>
+          </el-form-item>
+          <el-form-item label="组件用途" prop="note">
+            <el-input v-model="formInline.user.note" placeholder="组件用途"></el-input>
+          </el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+          <!--<a href="javascript:;" id="download"-->
+          <!--style="background-color:#409EFF;color: #fff;padding: 12px 10px!important;margin-left: 10px!important;border-radius:4px "-->
+          <!--@click="download()" download="download.csv">导出数据</a>-->
+        </el-form>
+        <!--表格-->
+        <el-table
+          :data="tableData"
+          border
+          style="width: 100%">
+          <!--<el-table-column type="selection">-->
+          <!--</el-table-column>-->
+          <el-table-column label="序号">
+            <template slot-scope="scope">
+              {{tableIndex(scope.$index)}}
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="type"
+            label="业务场景"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="servicerId"
+            label="服务标识"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="remark"
+            label="组件用途">
+          </el-table-column>
+          <!--<el-table-column label="操作">-->
+            <!--<template scope="scope">-->
+              <!--<el-button type="primary" size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>-->
+              <!--<el-button type="danger" size="small" @click="handleDelete(scope.$index, scope.row)">删除</el-button>-->
+            <!--</template>-->
+          <!--</el-table-column>-->
+        </el-table>
+        <div class="block">
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="pageObj.currentPage"
+            :page-size="pageObj.pageSize"
+            layout="prev, pager, next, jumper"
+            :total="pageObj.count">
+          </el-pagination>
+        </div>
+      </el-col>
+    </el-row>
+  </section>
+</template>
+<script type="text/ecmascript-6">
+  const ERR_OK = "000";
+  export default {
+    data() {
+      return {
+        formInline: {
+          user: {
+            serviceId: '',
+            note: ''
+          }
+        },
+        tableData: [],
+        pageObj: {
+          count: 0,
+          currentPage: 1,
+          pageSize: 20
+        },
+      };
+    },
+    created() {
+      this.getTslist();
+      this.$http.get('/api/getOptions').then((response) => {
+        response = response.data;
+        if (response.code === ERR_OK) {
+          this.options = response.datas;
+          this.places = response.places;
+        }
+      });
+    },
+    methods: {
+      getTslist() {
+        this.$http({
+          url: this.$http.adornUrl('/paas/manager/tslist'),
+          method: 'post',
+          data: this.$http.adornData(this.getRepParams())
+        }).then(({data}) => {
+          if (data && data.code === '10000') {
+            this.tableData = [].concat(data.data.list);
+            this.pageObj.currentPage = data.data.curPage;
+            this.pageObj.count = data.data.totalEl;
+          } else {
+            this.$message.error(data.msg)
+          }
+        })
+      },
+      onSubmit() {
+        // this.$message('模拟数据，这个方法并不管用哦~');
+        this.pageObj.currentPage = 1;
+        this.getTslist();
+        console.log("点击了查询方法")
+      },
+      handleDelete(index, row) {
+        this.tableData.splice(index, 1);
+        this.$message({
+          message: "操作成功！",
+          type: 'success'
+        });
+      },
+      handleEdit(index, row) {
+        this.dialogFormVisible = true;
+        this.form = Object.assign({}, row);
+        this.table_index = index;
+      },
+      handleSave() {
+        this.$confirm('确认提交吗？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          cancelButtonClass: 'cancel'
+        }).then(() => {
+          this.editLoading = true;
+          let date = this.form.date;
+          if (typeof date === "object") {
+            date = [date.getFullYear(), (date.getMonth() + 1), (date.getDate())].join('-');
+            this.form.date = date
+          }
+//          this.tableData[this.table_index] = this.form;
+          this.tableData.splice(this.table_index, 1, this.form);
+          this.$message({
+            message: "操作成功！",
+            type: 'success'
+          });
+          this.editLoading = false;
+          this.dialogFormVisible = false;
+        }).catch(() => {
+
+        });
+      },
+      download: function () {
+        console.log("xiazai")
+        var obj = document.getElementById('download');
+        var str = "姓名,出生日期,地址\n";
+        for (var i = 0; i < this.tableData.length; i++) {
+          var item = this.tableData[i];
+          str += item.name + ',' + item.date + ',' + item.address;
+          str += "\n";
+        }
+        console.log(obj)
+        str = encodeURIComponent(str);
+        console.log(str)
+        obj.href = "data:text/csv;charset=utf-8,\ufeff" + str;
+        obj.download = "download.csv";
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+        this.pageObj.pageSize = val;
+        this.getTslist();
+      },
+      handleCurrentChange(val) {
+        this.currentPage = val;
+        this.getTslist();
+        console.log(`当前页: ${val}`);
+      },
+      // table 序号
+      tableIndex (val = 0) {
+        const that = this
+        if (that.pageObj.currentPage < 2) {
+          return (val + 1)
+        } else {
+          return ((that.pageObj.currentPage - 1) * that.pageObj.pageSize + val + 1)
+        }
+      },
+      getRepParams() {
+        let obj = {}
+        obj.serviceId = this.formInline.user.serviceId || ''
+        obj.note = this.formInline.user.note || ''
+        obj.start = this.pageObj.currentPage || ''
+        obj.pagesize = this.pageObj.pageSize || ''
+        console.info('请求参数' + JSON.stringify(obj))
+        return obj
+      }
+    }
+  }
+  ;
+</script>
+<style>
+  .el-pagination {
+    text-align: center;
+    margin-top: 30px;
+  }
+
+  .el-message-box__btns .cancel {
+    float: right;
+    margin-left: 10px;
+  }
+</style>
